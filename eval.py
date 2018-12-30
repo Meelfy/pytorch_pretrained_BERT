2393d8f95a6e6ce3588651500751e1162933f20d
Meefly <admin@meijiex.vip>
2018-12-30 17:26:26 +0800

    no \n in eval.out

--- eval.py
+++ eval.py
@@ -10,6 +10,6 @@ for theta, alpha, beta in itertools.product(theta, alpha, beta):
     cmd.append("export SAVE_DIR=/tmp/SQuAD_v1-{0}_{1}_{2}_newloss/".format(theta, alpha, beta))
     cmd.append("python examples/evaluate-v1.1.py /data/nfsdata/meijie/data/SQuAD/dev-v1.1.json \
                 /tmp/SQuAD_v1-{0}_{1}_{2}_newloss/predictions.json >>eval.out".format(theta, alpha, beta))
-    cmd.append('echo "\n">>eval.out')
+    #cmd.append('echo "\n">>eval.out')
     cmd = ";".join(cmd)
     os.system(cmd)
